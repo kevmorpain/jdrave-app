8a020da87aca0d870e5ff0a75361581af23f1a21
Kévin Morpain <kevmorpain@gmail.com>
2022-05-09 23:05:49 +0200

    Add prod config for auth0

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,6 +1,7 @@
 import { createAuthPlugin, setupAuth } from '@/plugins/auth';
 import { Auth0ClientOptions } from '@auth0/auth0-spa-js';
 import authConfig from '../../auth_config.json';
+import authConfigDev from '../../auth_config.dev.json';
 import { RouteLocation } from 'vue-router';
 import router from '@/router';
 
@@ -9,7 +10,12 @@ const callbackRedirect = (appState: { targetUrl: RouteLocation }): void => {
 };
 
 function setupAuthPlugin(): Promise<void> {
-  return setupAuth(authConfig as Auth0ClientOptions, callbackRedirect);
+  return setupAuth(
+    (process.env.NODE_ENV === 'production'
+      ? authConfig
+      : authConfigDev) as Auth0ClientOptions,
+    callbackRedirect
+  );
 }
 
 export { createAuthPlugin, setupAuthPlugin };
