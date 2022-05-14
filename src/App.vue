7ca2a05375ffd6f25ca6d5d341a71e28222b54cc
Kévin Morpain <kevmorpain@gmail.com>
2022-05-15 01:34:10 +0200

    Log auth0 error

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,18 @@ import { apolloClient, updateApolloClientHeaders } from '@/plugins/apollo';
 import { useAuth0 } from '@auth0/auth0-vue';
 import { watchEffect } from 'vue';
 
-const { isAuthenticated, isLoading, getAccessTokenSilently } = useAuth0();
+const {
+  isAuthenticated,
+  isLoading,
+  getAccessTokenSilently,
+  error,
+} = useAuth0();
+
+watchEffect(() => {
+  if (error.value) {
+    console.log(error.value);
+  }
+});
 
 watchEffect(async () => {
   if (!isLoading.value && isAuthenticated.value) {
