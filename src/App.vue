19d37bfd06afe368f626da1bd907c4dfb6462764
Kévin Morpain <kevmorpain@gmail.com>
2022-05-15 02:01:40 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -6,28 +6,28 @@
 
 <script setup lang="ts">
 import Layout from '@/components/layouts/Default.vue';
-import { apolloClient, updateApolloClientHeaders } from '@/plugins/apollo';
-import { useAuth0 } from '@auth0/auth0-vue';
-import { watchEffect } from 'vue';
+// import { apolloClient, updateApolloClientHeaders } from '@/plugins/apollo';
+// import { useAuth0 } from '@auth0/auth0-vue';
+// import { watchEffect } from 'vue';
 
-const {
-  isAuthenticated,
-  isLoading,
-  getAccessTokenSilently,
-  error,
-} = useAuth0();
+// const {
+//   isAuthenticated,
+//   isLoading,
+//   getAccessTokenSilently,
+//   error,
+// } = useAuth0();
 
-watchEffect(() => {
-  if (error.value) {
-    console.log(error.value);
-  }
-});
+// watchEffect(() => {
+//   if (error.value) {
+//     console.log(error.value);
+//   }
+// });
 
-watchEffect(async () => {
-  if (!isLoading.value && isAuthenticated.value) {
-    const token = await getAccessTokenSilently();
-    updateApolloClientHeaders(token);
-    apolloClient.resetStore();
-  }
-});
+// watchEffect(async () => {
+//   if (!isLoading.value && isAuthenticated.value) {
+//     const token = await getAccessTokenSilently();
+//     updateApolloClientHeaders(token);
+//     apolloClient.resetStore();
+//   }
+// });
 </script>
