52d7704d68018e625c7db5f72e36488c197bb097
Kévin Morpain <kevmorpain@gmail.com>
2022-05-23 22:28:43 +0200

    Refresh app on update

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,28 @@
 import Layout from '@/components/layouts/DefaultLayout.vue';
 import { apolloClient, updateApolloClientHeaders } from '@/plugins/apollo';
 import { useAuth0 } from '@auth0/auth0-vue';
-import { watchEffect } from 'vue';
+import { ref, watchEffect } from 'vue';
+
+const isRefreshing = ref<boolean>(false);
+const updateApp = (e: CustomEvent): void => {
+  if (!e.detail || !e.detail.waiting) {
+    return;
+  }
+  e.detail.waiting.postMessage({ type: 'SKIP_WAITING' });
+};
+document.addEventListener('app-update', updateApp as (e: Event) => void, {
+  once: true,
+});
+
+// Refresh the app
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.addEventListener('controllerchange', () => {
+    if (!isRefreshing.value) {
+      isRefreshing.value = true;
+      window.location.reload();
+    }
+  });
+}
 
 const {
   isAuthenticated,
